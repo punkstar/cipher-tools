8dcba77b146d9856e0d78988a5558e619b619b5a
Nick Jones <nick@nicksays.co.uk>
2011-02-08 22:18:41 +0000

    A tool to generate the possible plaintexts of the shift cipher

---
+++ shift.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+
+# Problem Sheet 1: Shift Cipher
+# Assumptions: Plain text consists of lowercase letters only
+
+unless ARGV.length == 1
+  puts "usage: ./shift $CIPHERTEXT"
+  exit 1
+end
+
+ciphertext = ARGV[0]
+
+puts "Ciphertext: %s" % ciphertext
+
+(1..26).each do |i|
+  plaintext = ""
+  ciphertext.downcase.each_byte do |c|
+    new_c = ((c + i - 97) % 26 + 97).chr
+    plaintext = plaintext + new_c
+  end
+  
+  puts "Plaintext %d: %s" % [i, plaintext]
+end
